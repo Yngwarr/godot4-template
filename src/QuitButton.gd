2bb9bcacc0903d2224a524ea3a629fb473770124
Yngvarr <k.yngvarr@gmail.com>
2023-05-11 00:39:58 +0200

    removed quit button from web builds

--- src/QuitButton.gd
+++ src/QuitButton.gd
@@ -3,6 +3,8 @@ extends Button
 ## The only purpose of this button is to close the game.
 
 func _ready() -> void:
+	if OS.has_feature('web'):
+		visible = false
 	pressed.connect(quit)
 
 ## Closing the game in a most straightforward way.
